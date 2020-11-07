c8be646778d6cd8bc4f2c27071b09c76d605a440
Jacob Cowan <mrjcowman@gmail.com>
2020-11-06 20:39:44 -0800

    Set up employee state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, {useEffect, useState} from 'react';
+
+import EmployeeArray from './Utils/EmployeeArray';
 import EmployeeList from './Components/EmployeeList';
 import SearchForm from './Components/SearchForm';
+import API from './Utils/API';
 
 function App() {
+
+  const [myEmployees, setMyEmployees] = useState(new EmployeeArray());
+
+  useEffect(() => {
+    API.getNumOfUsers(20).then(data=>{
+      myEmployees.push(data.data.results);
+      console.log(myEmployees.employees);
+    }).catch(err=>{
+      throw err;
+    })
+  
+  })
+
+  function searchEmployees(event) {
+    return myEmployees.filterByName(event.query.value);
+  }
+
   return (
     <div className="App">
-      <SearchForm />
-      <EmployeeList />
+      <SearchForm onSubmit={searchEmployees} />
+      <EmployeeList employees={myEmployees}/>
     </div>
   );
 }
